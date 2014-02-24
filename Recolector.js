23486689a421f8b64e286bba9a6b5df8df96f54f
soysalem <soysalem@hotmail.com>
2014-02-24 13:08:09 +0100

    actualizacion
    se deja como estaba

--- Recolector.js
+++ Recolector.js
@@ -11,7 +11,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
-/*bla bla bla bla bla bla */
+
 var http = require('http'),
     url = require('url'),
 	contador = 0,
